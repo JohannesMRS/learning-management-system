da1badceadf1066f7299060316d7023d4c5f72ac
Johannes <mariojr8304@gmail.com>
2026-06-15 15:47:02 +0700

    Feat: Adding Style For Login & Registration Pages

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -6,13 +6,13 @@
     <title>Document</title>
     @vite('resources/css/app.css')
 </head>
-<body class = "flex justify-center items-center h-screen font-mono font-bold" autocomplete = "off">
+<body class = "flex justify-center items-center h-screen font-mono" autocomplete = "off">
     <div class = "w-4/12 pb-10  flex flex-col shadow-[0px_1px_8px_1px_rgba(0,0,0,0.2)] rounded-2xl">
-        <h1 class = "text-2xl text-center  mt-6 mb-6">Form Login</h1>
+        <h1 class = "bg-linear-to-r from-blue-500 to-purple-500  p-8  text-center  rounded-t-2xl mb-6 font-bold text-white text-[30px]">Form Login</h1>
         <form action="{{ route('loginProses') }}" method = "POST">
             @csrf
             @if ($errors->any())
-                <div style="background-color: #fee2e2; color: #dc2626; padding: 10px; border-radius: 8px; margin-bottom: 15px;">
+                <div  class = "bg-[#fee2e2] text-[#dc2626] p-2.5 rounded-lg mb-3.75 ml-3.75 mr-3.75 text-center">
                     <ul>
                         @foreach ($errors->all() as $error)
                             <li>{{ $error }}</li>
@@ -21,18 +21,18 @@
                 </div>
             @endif
             <div class = "flex flex-col pl-8 pr-8">
-                <label for="email" class = "mb-2">Email</label>
-                <input type="text" name = "email" placeholder = "Masukkan email" class = "border border-gray-400 rounded h-10 pl-3">
+                <label for="email" class = "mb-2 text-gray-600">Email</label>
+                <input type="text" autocomplete = "off" name = "email" placeholder = "Masukkan email" class = "border border-gray-400 text-gray-600 rounded h-10 pl-3 focus:blue-500 focus:ring-3 focus:ring-blue-200 focus:outline-none">
             </div>
             <div class = "flex flex-col pl-8 pr-8 mt-5">
-                <label for="password" class = "mb-2">Password</label>
-                <input type="password" name = "password" placeholder = "Masukkan password" class = "border border-gray-400 rounded h-10 pl-3">
+                <label for="password" class = "mb-2 text-gray-600">Password</label>
+                <input type="password" autocomplete = "new-password" name = "password" placeholder = "Masukkan password" class = "border border-gray-400 text-gray-600 rounded h-10 pl-3 focus:border-blue-500 focus:ring-3 focus:ring-blue-200 focus:outline-none">
             </div>
-            <button type = "submit"  class = "w-10/12 h-10 block mx-auto mt-10 bg-amber-400">Login</button>
+            <button type = "submit"  class = "w-10/12 h-10 block mx-auto mt-10 bg-blue-500 cursor-pointer text-white rounded hover:bg-purple-500 duration-100">Login</button>
         </form>
 
-        <p>
-            Belum punya akun? <a href="{{ route('register') }}">Daftar Disini</a>
+        <p class = "text-center mt-10">
+            Belum punya akun? <a href="{{ route('register') }}" class = "text-blue-500 hover:text-purple-500 focus:text-purple-500 duration-200 hover:underline">Daftar Disini</a>
         </p>
     </div>
 </body>
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -4,12 +4,12 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Register - LMS Mentoring</title>
-    <script src="https://cdn.tailwindcss.com"></script>
+    @vite('resources/css/app.css')
 </head>
-<body class="bg-gray-100 flex items-center justify-center h-screen">
+<body class=" flex items-center justify-center h-screen font-mono">
 
-    <div class="bg-white p-8 rounded-xl shadow-md w-96 border border-gray-100">
-        <h2 class="text-2xl font-bold text-center mb-6 text-gray-800 tracking-wide">Form Register</h2>
+    <div class="w-4/12 bg-white  rounded-xl shadow-md  border border-gray-100 pb-10">
+        <h1 class="text-2xl font-bold text-center mb-6 p-8 rounded-t-xl tracking-wide bg-linear-to-r from-blue-500 to-purple-500 text-white">Form Register</h1>
 
         @if ($errors->any())
             <div class="bg-red-100 border border-red-200 text-red-600 px-4 py-2 rounded-lg mb-4 text-sm font-medium">
@@ -24,62 +24,63 @@
         <form id = "formRegister" action="{{ route('registerProses') }}" method="POST" autocomplete="off" class="space-y-4">
             @csrf
 
-            <div>
-                <label class="block text-sm font-semibold text-gray-600 mb-1">Nama Lengkap</label>
+            <div class = "pl-5 pr-5">
+                {{-- <label class="block text-sm font-semibold text-gray-600 mb-1">Nama Lengkap</label> --}}
                 <input type="text"
                        name="name"
                        required
-                       placeholder="Masukkan nama lengkap"
+                       placeholder="Nama Lengkap"
                        value="{{ old('name') }}"
-                       class="w-full px-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-amber-500 focus:border-amber-500 transition duration-200">
+                       class="w-full px-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-200 focus:border-blue-500 transition duration-200">
             </div>
 
-            <div>
-                <label class="block text-sm font-semibold text-gray-600 mb-1">Email</label>
+            <div class = "pl-5 pr-5">
+                {{-- <label class="block text-sm font-semibold text-gray-600 mb-1">Email</label> --}}
                 <input type="email"
                        name="email"
                        required
-                       placeholder="Masukkan email"
+                       placeholder="Email"
                        value="{{ old('email') }}"
-                       class="w-full px-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-amber-500 focus:border-amber-500 transition duration-200">
+                       class="w-full px-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-200 focus:border-blue-500 transition duration-200">
             </div>
 
-            <div>
-                <label class="block text-sm font-semibold text-gray-600 mb-1">Password</label>
+            <div class = "pl-5 pr-5">
+                {{-- <label class="block text-sm font-semibold text-gray-600 mb-1">Password</label> --}}
                 <input type="password"
                        name="password"
                        required
-                       placeholder="Minimal 6 karakter"
-                       class="w-full px-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-amber-500 focus:border-amber-500 transition duration-200">
+                       placeholder="Password"
+                       class="w-full px-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-200 focus:border-blue-500 transition duration-200">
             </div>
 
-            <div>
-                <label class="block text-sm font-semibold text-gray-600 mb-1">Konfirmasi Password</label>
+            <div class = "pl-5 pr-5">
+                {{-- <label class="block text-sm font-semibold text-gray-600 mb-1">Konfirmasi Password</label> --}}
                 <input type="password"
                     name="password_confirmation"
+                    autocomplete = "new-password"
                     required
-                    placeholder="Ulangi password kamu"
-                    class="w-full px-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-amber-500 focus:border-amber-500 transition duration-200">
+                    placeholder="Konfirmasi Password"
+                    class="w-full px-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-200 focus:border-blue-500 transition duration-200">
             </div>
 
-            <div>
-                <label class="block text-sm font-semibold text-gray-600 mb-1">Kode Registrasi (Token Admin)</label>
+            <div class = "pl-5 pr-5">
+                {{-- <label class="block text-sm font-semibold text-gray-600 mb-1">Kode Registrasi (Token Admin)</label> --}}
                 <input type="text"
                        name="registration_code"
                        required
-                       placeholder="Contoh: TOKEN-XYZ"
-                       class="w-full px-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-amber-500 focus:border-amber-500 transition duration-200 uppercase font-mono tracking-wider">
+                       placeholder="Kode Registrasi"
+                       class="w-full px-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-200 focus:border-blue-500 transition duration-200 uppercase font-mono tracking-wider">
             </div>
 
 
             <button type="submit"
-                    class="w-full bg-amber-500 text-white py-2.5 rounded-lg font-bold hover:bg-amber-600 transition duration-200 shadow-sm mt-2">
+                    class="w-11/12 block mx-auto bg-blue-500 hover:bg-purple-500 duration-200 text-white py-2.5 rounded font-bold transition shadow-sm mt-2 cursor-pointer">
                 Register
             </button>
         </form>
 
         <p class="text-sm text-center text-gray-500 mt-5">
-            Sudah punya akun? <a href="{{ route('login') }}" class="text-amber-600 font-semibold hover:underline">Login disini</a>
+            Sudah punya akun? <a href="{{ route('login') }}" class="text-blue-500  font-semibold hover:text-purple-500 hover:underline">Login disini</a>
         </p>
     </div>
     <script>
