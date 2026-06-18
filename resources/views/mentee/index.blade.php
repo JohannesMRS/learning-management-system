57d7dfb8ec438c9aa10c1cf5236363adbb66a8f2
Johannes <mariojr8304@gmail.com>
2026-06-18 08:17:37 +0700

    Feat: Adding Style For Home Mentee Page

--- resources/views/mentee/index.blade.php
+++ resources/views/mentee/index.blade.php
@@ -1,48 +1,3 @@
-{{-- <!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    @vite('resources/css/app.css')
-</head>
-<body> --}}
-
-    {{-- <nav class="flex h-16 justify-between p-5 pl-15 pr-10 items-center shadow-[2px_0px_5px_2px_rgba(128,128,128,0.2)]">
-    <div class = "text-xl font-bold font-mono bg-linear-to-r from-blue-500 to-purple-500 text-transparent bg-clip-text">
-        Mentoring
-    </div>
-    <div class="flex gap-10">
-        <a href="#">Home</a>
-        <a href="#">Module</a>
-    </div>
-
-    <!-- Bungkus Tombol dan Menu di dalam satu div 'relative' -->
-    <div class="relative">
-        <button id="dropDownButton" class="flex flex-col h-10 w-10 justify-center items-center focus:outline-none cursor-pointer rounded-full bg-gray-200 hover:bg-gray-400 transition duration-200 font-mono">
-            <span class="font-semibold text-xl text-gray-800">{{ Str::of(auth()->user()->name)->headline()->explode(' ')->map(fn($word)=>mb_substr($word, 0, 1))->take(2)->implode('') }}</span>
-
-        </button>
-
-        <!-- Menu Dropdown (Posisinya absolute agar melayang) -->
-        <div id="dropDownMenu" class="hidden absolute right-0 mt-2 w-48 bg-white rounded-md shadow-lg py-1 z-50 border border-gray-100">
-            <span class="text-sm text-gray-500 ml-4">{{ auth()->user()->name }}</span>
-            <form action="{{ route('logout') }}" method="POST" class="block w-full">
-                @csrf
-                <button type="submit" class="w-full text-left px-4 py-2 text-sm text-red-600 hover:bg-gray-100 transition-colors">
-                    Logout
-                </button>
-            </form>
-        </div>
-    </div>
-</nav> --}}
-
-    {{-- <form action="{{ route('logout') }}" method = "POST">
-        @csrf
-        <button type = "submit">
-            Logout
-        </button>
-    </form> --}}
 
 @extends('layouts.navbar')
 
@@ -50,8 +5,12 @@
 
 @section('content')
 
-    <div>Hello</div>
+    <section class="">
+        <div class = "w-full h-screen flex flex-col justify-center item-center">
+            <h1 class = "text-center text-4xl font-bold">Selamat Datang Kembali <span class = "text-purple-500">{{ Auth::user()->name }}</span></h1>
+            <p class = "text-center mt-20 "><a href="{{ route('mentee.module') }}" class = "bg-linear-to-r from-purple-500 to-blue-500 p-2 text-white hover:bg-blue-500">Lanjut Belajar</a></p>
+        </div>
+    </section>
 
 @endsection
-{{-- </body>
-</html> --}}
+
